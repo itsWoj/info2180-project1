646a5ca675fa0d1e119b45d6e0e71f798ac10d17
ItsWoj <itzparkes@gmail.com>
2023-11-03 07:02:20 -0500

    completed website

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var form = document.querySelector('form');
+
+    var messageElement = document.querySelector('.message');
+
+    form.addEventListener('submit', function(event) {
+
+        event.preventDefault();
+
+        var email = document.getElementById('email').value;
+
+        if (email === '' || !isValidEmail(email)) {
+         
+            messageElement.textContent = "Please enter a valid email address.";
+        } else {
+        
+            messageElement.textContent = "Thank you! Your email address " + email + " has been added to our mailing list!";
+        }
+    });
+
+
+    function isValidEmail(email) {
+        var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return pattern.test(email);
+    }
+
+});
